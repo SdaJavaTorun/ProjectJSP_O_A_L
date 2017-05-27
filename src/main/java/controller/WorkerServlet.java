a935925ac2dabc625078a2150a498f88636d6d24
olwj <al.wajgert@gmail.com>
2017-05-27 11:05:40 +0200

    first servlet

--- src/main/java/controller/WorkerServlet.java
+++ src/main/java/controller/WorkerServlet.java
@@ -1,4 +1,22 @@
 package controller;
 
-public class WorkerServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class WorkerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        resp.setContentType("text/html");
+        HttpSession session = req.getSession(true);
+
+
+        req.setAttribute("board", session );
+        req.getRequestDispatcher("/InsertWorker.jsp").forward(req, resp);
+}
 }
